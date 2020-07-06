e795a2249c3699b307ab75fc5bb4106b494565ef
Michael DeMarco <michaelfromyeg@gmail.com>
2020-07-06 11:51:24 -0600

    Research gathered correctly, debugging data added, simple write to output

--- src/research.py
+++ src/research.py
@@ -6,23 +6,25 @@ from scholarly import scholarly
 from typing import List, NamedTuple
 import sys, getopt
 import csv
+import shelve
 
 # Data definitions
 
 class Data(NamedTuple):
-  course: str
-  prof: str
-  email: str
-
-class Publication(NamedTuple):
-  year: int
-  abstract: str
-  title: str
-  authors: str
+  lab: str
+  lab_id: str
   url: str
 
+class Publication(NamedTuple):
+  title: str
+  authors: str
+  year: int
+  citations: int
+  publisher: str
+
 class Research(NamedTuple):
-  prof: str
+  lab: str
+  lab_id: str
   publications: List[Publication]
 
 class bcolors:
@@ -35,13 +37,13 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-debug = False
+debug = True
 
 # Functions
 
 def main(filenames: [str, str]) -> None:
   '''
-  Read data, anlyze it, and output it
+  Read data, analyze it, and output it
   '''
   input_file = filenames[0]
   output_file = filenames[1]
@@ -57,7 +59,7 @@ def read(filename: str) -> List[Data]:
   lod = [] # type: List[Data]
 
   try:
-    with open(f'../data/{filename}') as csv_file:
+    with open (f'../data/{filename}') as csv_file:
 
       # Setup CSV reader
       csv_reader = csv.reader(csv_file, delimiter=',')
@@ -67,16 +69,14 @@ def read(filename: str) -> List[Data]:
       for row in csv_reader:
           # Parse headers
           if line_count == 0:
-              print(f'{bcolors.WARNING}Column names are {", ".join(row)}. 
-                You should have Course, Name, Email (in that order); 
-                if you do not, please consult the README.md.{bcolors.ENDC}')
+              print(f'{bcolors.WARNING}Column names are {", ".join(row)}. You should have Lab, ID, URL (in that order); if you do not, please consult the README.md.{bcolors.ENDC}')
               line_count += 1
           # Append row to lod
           else:
               if (debug):
-                print(f'{bcolors.OKBLUE}Course: {row[0]}\tProfessor: {row[1]}\tEmail: {row[2]}{bcolors.ENDC}')
-              r = Data(row[0], row[1], row[2])
-              lod.append(r)
+                print(f'{bcolors.OKBLUE}Lab: {row[0]}\tID: {row[1]}\tURL: {row[2]}{bcolors.ENDC}')
+              d = Data(row[0], row[1], row[2])
+              lod.append(d)
               line_count += 1
       print(f'{bcolors.OKGREEN}Processed {line_count} lines... now gathering research!{bcolors.ENDC}')
   
@@ -98,21 +98,27 @@ def research(lod: List[Data]) -> List[Research]:
   d: Data
   for d in lod:
     if (debug):
-      print(f'Trying {d.prof}...')
-    search_query = scholarly.search_author(d.prof)
-    author = next(search_query).fill()
-    
+      print(f'Trying {d.lab}...')
+
+    lab = None
+    if (debug):
+      data = shelve.open('data')
+      lab = data['biot']
+    else:
+      lab = scholarly.search_author_id('ZImFmCUAAAAJ', True)
+      
     # Compile all publications
     lop = [] # List[Publication]
     # p's type is given by scholarly
-    for p in author.publications[:2]:
+    for p in lab.publications[:2]:
       p.fill()
+      print(p)
       bib = p.bib
-      custom_pub = Publication(int(bib['year']), bib['abstract'], bib['title'], bib['author'], bib['url'])
+      custom_pub = Publication(bib['title'], bib['author'], int(bib['year']), get_citations(p.cites_per_year), bib['publisher'])
       lop.append(custom_pub)
     
     # Attach professor to publications
-    r = Research({d.prof},lop)
+    r = Research({d.lab}, {d.lab_id}, lop)
     lor.append(r)
   return lor
 
@@ -120,9 +126,25 @@ def generate(lod: List[Data], output_file: str) -> None:
   '''
   Generate csv file from given research
   '''
+  # Get research from data
   lor = research(lod)
-  print(lor[0])
+
+  # Write list of research data to a csv file
+  o = open(f'../data/{output_file}', 'w')
+  with o:
+    writer = csv.writer(o)
+    writer.writerow(['Lab', 'Lab ID', 'Publications'])
+    for row in lor:
+      print(row)
+      writer.writerow(row)
+
   return None
+
+def get_citations(cites_per_year: dict) -> int:
+  '''
+  Helper function to process cites per year to a single number
+  '''
+  return sum(cites_per_year.values())
 
 def get_args(argv: List[str]) -> [str, str]:
   input_file = ''
@@ -130,11 +152,11 @@ def get_args(argv: List[str]) -> [str, str]:
   try:
     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
   except getopt.GetoptError:
-    print('research.py -i <inputfile> -o <outputfile>')
+    print('python research.py -i <inputfile> -o <outputfile>')
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print('test.py -i <inputfile> -o <outputfile>')
+      print('python research.py -i <inputfile> -o <outputfile>')
       sys.exit()
     elif opt in ("-i", "--input"):
       input_file = arg
